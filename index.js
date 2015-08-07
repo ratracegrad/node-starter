52f87d56d4ee0ced230269bbd77cdc5840512a9e
Stephen Rodriguez <steprodriguez10@gmail.com>
2015-08-07 01:16:08 -0400

    renaming the folder heirarchy

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
+// Required Modules
 var express = require('express');
+var cors = require('cors');
+
 var app = express();
 
-//##############################################
+//###############################################
 
 // Custom Route Mapping
 app.use('/login', require('./src/login/Login'));
 
-//##############################################
+//###############################################
 
+// Setting views folder/engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 app.engine('html', require('ejs').renderFile);
 
-//##############################################
+// Including our resources
+app.use(express.static('app'));
+app.use(express.static('assets'));
+
+//###############################################
 
 // Home Page
 app.get('/', function(req, res) {
 	res.render('index.html');
 });
 
-//##############################################
+//###############################################
 
 var port = (process.env.PORT || 5000);
 app.set('port', port);
