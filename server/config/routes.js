676f77c7a492e6153db0626000e70d8ecd1690c7
Stephn Rodriguez <steprodriguez10@gmail.com>
2015-08-14 02:11:03 -0400

    sorting

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,10 +2,10 @@
 
 module.exports = function(app) {
 
-  var path = require('path');
-  var logger = app.locals.logger;
-  var views = app.locals.config.views.path;
   var ext = app.locals.config.views.ext;
+  var logger = app.locals.logger;
+  var path = require('path');
+  var views = app.locals.config.views.path;
 
   app.get("/", function (req, res) {
     logger.info('Returning index.html...');
